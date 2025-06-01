886f7a8d6cd8d741a4a638e4458bb59ad184ffa4
Santhosh13r <162345493+Santhosh13r@users.noreply.github.com>
2025-06-01 20:48:33 +0530

    pages update

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -64,7 +64,7 @@ const CarouselComponent = () => {
               color: "#fff",
               fontWeight: "bold",
               textShadow: "2px 2px 8px #000"
-            }}>N</span>
+            }}>Enterinment News..</span>
           </div>
         </div>
 
